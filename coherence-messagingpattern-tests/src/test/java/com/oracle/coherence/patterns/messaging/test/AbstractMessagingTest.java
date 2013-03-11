f369877a512f3b3b10838a6d6adbca1b3c823803
Brian Oliver <brian.oliver@oracle.com>
2013-03-11 15:26:40 -0400

    Resolves Issue COHINC-28: Introduced coherence-messagingpattern-tests module to contain messaging functional tests (http://java.net/jira/browse/COHINC-28) Resolves Issue COHINC-33: Upgraded to use JUnit 4.11 and Hamcrest 1.3 (http://java.net/jira/browse/COHINC-33)

---
+++ coherence-messagingpattern-tests/src/test/java/com/oracle/coherence/patterns/messaging/test/AbstractMessagingTest.java
@@ -0,0 +1,102 @@
+/*
+ * File: AbstractMessagingTest.java
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * The contents of this file are subject to the terms and conditions of 
+ * the Common Development and Distribution License 1.0 (the "License").
+ *
+ * You may not use this file except in compliance with the License.
+ *
+ * You can obtain a copy of the License by consulting the LICENSE.txt file
+ * distributed with this file, or by consulting
+ * or https://oss.oracle.com/licenses/CDDL
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ * When distributing the software, include this License Header Notice in each
+ * file and include the License file LICENSE.txt.
+ *
+ * MODIFICATIONS:
+ * If applicable, add the following below the License Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyright [year] [name of copyright owner]"
+ */
+
+package com.oracle.coherence.patterns.messaging.test;
+
+import com.oracle.coherence.patterns.messaging.DefaultMessagingSession;
+import com.oracle.coherence.patterns.messaging.MessagingSession;
+
+import com.oracle.tools.junit.AbstractTest;
+
+import com.oracle.tools.runtime.java.virtualization.Virtualization;
+
+import com.oracle.tools.runtime.network.Constants;
+
+import org.junit.After;
+import org.junit.Before;
+
+import java.io.IOException;
+
+/**
+ * The base class for all Messaging Pattern Functional Tests.
+ * <p>
+ * Copyright (c) 2013. All Rights Reserved. Oracle Corporation.<br>
+ * Oracle is a registered trademark of Oracle Corporation and/or its affiliates.
+ *
+ * @author Brian Oliver
+ */
+public abstract class AbstractMessagingTest extends AbstractTest
+{
+    /**
+     * The MessagingSession to be used by all tests.
+     */
+    private MessagingSession m_messagingSession;
+
+
+    /**
+     * Acquire the MessagingSession to use for the tests.
+     *
+     * @return the MessagingSession
+     */
+    public MessagingSession getMessagingSession()
+    {
+        return m_messagingSession;
+    }
+
+
+    /**
+     * Setup the Test
+     */
+    @Before
+    public void setup() throws IOException
+    {
+        System.setProperty("tangosol.coherence.cacheconfig", "coherence-messagingpattern-test-cache-config.xml");
+        System.setProperty("tangosol.pof.config", "coherence-messagingpattern-test-pof-config.xml");
+        System.setProperty("tangosol.coherence.clusterport", "" + Virtualization.getAvailablePorts().next());
+        System.setProperty("tangosol.coherence.localhost", "" + Constants.getLocalHost());
+
+//      // touch all of the messaging caches to get them started
+//      CacheFactory.getCache("coherence.messagingpattern.subscriptions");
+//      CacheFactory.getCache("coherence.messagingpattern.messages");
+//      CacheFactory.getCache("coherence.messagingpattern.destinations");
+
+        // create a shared MessagingSession for the tests
+        m_messagingSession = DefaultMessagingSession.getInstance();
+    }
+
+
+    /**
+     * Teardown the Test
+     */
+    @After
+    public void shutdown()
+    {
+//      // shutdown our CacheFactory
+//      CacheFactory.shutdown();
+//
+//      Virtualization.stop();
+    }
+}
